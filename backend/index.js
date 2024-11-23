78dc89e62b53208544f163c914ff2b8653f04b7c
Komal <komalhasija4020@gmail.com>
2024-11-23 12:40:45 +0530

    uuuo

--- backend/index.js
+++ backend/index.js
@@ -21,7 +21,7 @@ app.use(cookieParser());
 
 
 const corsOptions = {
-  origin: ['http://localhost:5173', 'http://localhost:5173/'], // Allow both variants
+  origin: ['https://jobhunt-2-174m.onrender.com', 'https://jobhunt-2-174m.onrender.com/'], // Allow both variants
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true,
 };
